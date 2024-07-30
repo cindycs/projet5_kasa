43c22deb7626be43c8435b12395962d5e2548c6d
cindycs <schaal.cindy@gmail.com>
2024-07-30 10:03:24 +0200

    header page d'accueil

--- projet-react-kasa/src/Pages/accueil.jsx
+++ projet-react-kasa/src/Pages/accueil.jsx
@@ -1,9 +1,11 @@
 import Home from "../components/home/Home"
 import Gallery from "../components/Gallery"
+import Carrousel from "../components/Carrousel"
 
 export default function Accueil() {
     return (
         <div className="wrapper">
+            <Carrousel />
             <Home />
             <Gallery />
         </div>
--- projet-react-kasa/src/components/Header/index.jsx
+++ projet-react-kasa/src/components/Header/index.jsx
@@ -1,15 +1,17 @@
-import Carrousel from '../Carrousel'
 import { Link } from 'react-router-dom'
+import Logo from '../../assets/logo.png'
 
 
 export default function Header() {
     return (
-        <header className='wrapper'>
+        <header className='wrapper-header'>
+            <div className='logo'>
+                <img src={Logo} alt="Logo Kasa"/>
+            </div>
             <nav>
-                <Link to="/">Accueil</Link>
-                <Link to="/a-propos">A-propos</Link>
+                <Link className='active' to="/">Accueil</Link>
+                <Link to="/a-propos">A propos</Link>
             </nav>
-            <Carrousel />
         </header>
     )
 }
