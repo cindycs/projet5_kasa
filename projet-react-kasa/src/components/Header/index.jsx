f36ce8f774d9899acea05d4078077ca56f8fd002
cindycs <schaal.cindy@gmail.com>
2024-08-05 17:01:38 +0200

    la class "active" change dans le composant Header selon la page selectionné

--- projet-react-kasa/src/components/Header/index.jsx
+++ projet-react-kasa/src/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'; //Extension de Link
 import Logo from '../../assets/logo.png'
 
 
@@ -9,8 +9,19 @@ export default function Header() {
                 <img src={Logo} alt="Logo Kasa"/>
             </div>
             <nav>
-                <Link className='active nav-contain' to="/">Accueil</Link>
-                <Link className='nav-contain' to="/a-propos">A propos</Link>
+                <NavLink 
+                    to="/" 
+                    end //s'assure que la classe active est appliqué ubiquement lorsque la correspond exactement a /
+                    className={({ isActive }) => isActive ? "nav-contain active" : "nav-contain"}
+                >
+                    Accueil
+                </NavLink>
+                <NavLink 
+                    to="/a-propos" 
+                    className={({ isActive }) => isActive ? "nav-contain active" : "nav-contain"}
+                >
+                    A propos
+                </NavLink>
             </nav>
         </header>
     )
