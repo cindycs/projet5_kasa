060bee7b6b9ffb17cbb7011683f1247e87f3ae4e
cindycs <schaal.cindy@gmail.com>
2024-08-09 11:48:52 +0200

    creation du composant Slideshow avec son style

--- projet-react-kasa/src/Pages/fiche-logement.jsx
+++ projet-react-kasa/src/Pages/fiche-logement.jsx
@@ -1,12 +1,13 @@
 import logementList from '../data/logement.json';
 import { useParams } from "react-router-dom";
-
+import Slideshow from '../components/Slideshow';
 
 export default function FicheLogement() {
     const { id } = useParams();
 
     return (
         <div className="wrapper">
+            <Slideshow />
             <h2>Ceci est la fiche de logement n°{id}</h2>
             <section className='logement'>
             {logementList.map((logement) => (
