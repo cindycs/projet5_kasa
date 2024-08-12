e22d8e599fab860560eb109167d9d09d7a09c5ad
cindycs <schaal.cindy@gmail.com>
2024-08-12 10:53:24 +0200

    slideshow fonctionnel en statique

--- projet-react-kasa/src/components/Slideshow/index.jsx
+++ projet-react-kasa/src/components/Slideshow/index.jsx
@@ -12,22 +12,23 @@ export default function Slideshow() {
         "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-3.jpg"
     ];
 
+    //Fonctions qui modifient l'index actuel
     const handlePrev = () => {
-        setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex -1));
+        setCurrentIndex((currentIndex) => (currentIndex === 0 ? images.length - 1 : currentIndex - 1));
     }
 
     const handleNext = () => {
-        setCurrentIndex((nextIndex) => (nextIndex === images.lenght ? 0 : prevIndex +1));
+        setCurrentIndex((currentIndex) => (currentIndex === images.length - 1 ? 0 : currentIndex + 1));
     }
 
 
     return (
         <div className="slide-show">
-            <button className="prev" onClick={() => setPrevious(!previous)}><FontAwesomeIcon icon={faChevronLeft} /></button>
-            <button className="next" onClick={() => setNext(!next)}><FontAwesomeIcon icon={faChevronRight} /></button>
+            <button className="prev" onClick={handlePrev}><FontAwesomeIcon icon={faChevronLeft} /></button>
+            <button className="next" onClick={handleNext}><FontAwesomeIcon icon={faChevronRight} /></button>
             <ul>
                 {images.map((image, index) => (
-                    <li key={index} className="slide active">
+                    <li key={index} className={`slide ${index === currentIndex ? 'active' : ''}`}>
                         <img src={image}/>
                     </li>
                 ))}
