3c600f541ea1f01f0ec15c66b00f7148525ad1a6
cindycs <schaal.cindy@gmail.com>
2024-08-13 10:47:55 +0200

    fonctionnalité de redirection lorsque l'id n'est pas bonne

--- projet-react-kasa/src/Pages/fiche-logement.jsx
+++ projet-react-kasa/src/Pages/fiche-logement.jsx
@@ -1,10 +1,10 @@
 import logementList from '../data/logement.json';
-import { useState } from "react";
 import { useParams } from "react-router-dom";
 import Slideshow from '../components/Slideshow';
 import Collapse from '../components/Collapse';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { Navigate } from "react-router-dom";
 
 export default function FicheLogement() {
     const { id } = useParams(); //Récupération de l'id de l'URL
@@ -13,50 +13,57 @@ export default function FicheLogement() {
 
     return (
         <div className="wrapper">
-            <Slideshow pictures={logement.pictures}/>
-            <section className='logement'>
-                <div className="logement-title">
-                    <h2>{logement.title}</h2>
-                    <p className="localisation">{logement.location}</p>
-                    <ul className='list-tag'>
-                        {logement.tags.map((tag, index) => (
-                            <li className="tag" key={index}>
-                                {tag}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                <div className="logement-autor">
-                    <div className="logement-host">
-                        <div className='host-name'>
-                            <p>{logement.host.name}</p>
+            {logement ? (
+                <div>
+                    <Slideshow pictures={logement.pictures}/>
+                    <section className='logement'>
+                        <div className="logement-title">
+                            <h2>{logement.title}</h2>
+                            <p className="localisation">{logement.location}</p>
+                            <ul className='list-tag'>
+                                {logement.tags.map((tag, index) => (
+                                    <li className="tag" key={index}>
+                                        {tag}
+                                    </li>
+                                ))}
+                            </ul>
                         </div>
-                        <div className='host-picture'>
-                            <img src={logement.host.picture} alt={`photo de profil de ${logement.host.name}`}/>
+                        <div className="logement-autor">
+                            <div className="logement-host">
+                                <div className='host-name'>
+                                    <p>{logement.host.name}</p>
+                                </div>
+                                <div className='host-picture'>
+                                    <img src={logement.host.picture} alt={`photo de profil de ${logement.host.name}`}/>
+                                </div>
+                            </div>
+                            <div className="logement-rating">
+                                <ul className='list-stars'>
+                                {Array.from({ length: totalStars }, (_, index) => (
+                                    <li
+                                        key={index}
+                                        className={`stars ${index < logement.rating ? 'filled' : ''}`}
+                                    >
+                                        <FontAwesomeIcon icon={faStar} />
+                                    </li>
+                                ))}
+                                </ul>
+                            </div>
+                        </div>
+                    </section>
+                    <div className="logement-contain">
+                        <div className="logement-carac">
+                            <Collapse title="Description" description={logement.description}/>
+                        </div>
+                        <div className="logement-carac">
+                            <Collapse title="Équipements" description={logement.equipments}/>
                         </div>
                     </div>
-                    <div className="logement-rating">
-                        <ul className='list-stars'>
-                        {Array.from({ length: totalStars }, (_, index) => (
-                            <li
-                                key={index}
-                                className={`stars ${index < logement.rating ? 'filled' : ''}`}
-                            >
-                                <FontAwesomeIcon icon={faStar} />
-                            </li>
-                        ))}
-                        </ul>
-                    </div>
                 </div>
-            </section>
-            <div className="logement-contain">
-                <div className="logement-carac">
-                    <Collapse title="Description" description={logement.description}/>
-                </div>
-                <div className="logement-carac">
-                    <Collapse title="Équipements" description={logement.equipments}/>
-                </div>
-            </div>
+            ) : (
+                <Navigate to="/404" />
+            )}
+            
         </div>
     )
 }
